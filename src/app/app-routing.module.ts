2b64ea6a4d3ea610c31688a88b582aa64eb2f0f4
Olabode Opapeju <olaboe00@gmail.com>
2019-02-26 16:31:20 -0500

    Initial setup and routing of home page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeBaseComponent } from './components/home/home-base/home-base.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeBaseComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/general/navbar/navbar.component';
+import { FooterComponent } from './components/general/footer/footer.component';
+import { HomeBaseComponent } from './components/home/home-base/home-base.component';
+import { TopFilterComponent } from './components/general/top-filter/top-filter.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    HomeBaseComponent,
+    TopFilterComponent
   ],
   imports: [
     BrowserModule,
